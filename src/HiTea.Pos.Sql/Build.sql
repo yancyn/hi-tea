8069dd2c48e133cdcb66126ac921754412ed2ab0
yancyn <yancyn@gmail.com>
2013-10-22 00:28:37 +0800

    Change sql build script to pascal naming convention.
    To suit .NET style.

--- src/HiTea.Pos.Sql/Build.sql
+++ src/HiTea.Pos.Sql/Build.sql
@@ -1,95 +1,104 @@
 ﻿-- This is SQLite build script
 PRAGMA foreign_keys = ON;
 
---drop table ordercharges;
---drop table orderitems;
---drop table orders;
---drop table ordertypes;
---drop table charges;
---drop table menus;
---drop table statuses;
---drop table categories;
---drop table users;
---drop table roles;
+--drop table OrderItems;
+--drop table Orders;
+--drop table OrderTypes;
+--drop table Charges;
+--drop table Menus;
+--drop table Statuses;
+--drop table Categories;
+--drop table Users;
+--drop table Roles;
 
-create table if not exists roles(
-	role varchar(15) primary key
+CREATE TABLE IF NOT EXISTS Roles(
+	Id integer primary key autoincrement,
+	Name varchar(15)
 );
 
-create table if not exists users(
-	username varchar(30) primary key,
-	password text,
-	role varchar(15),
-	displayname text,
-	street1 text,
-	street2 text,
-	postcode text,
-	city text,
-	state text,
-	country text,
-	point integer,
-	foreign key(role) references roles(role)
+CREATE TABLE IF NOT EXISTS Users(
+	Id integer primary key autoincrement,
+	Username varchar(30),
+	Password text,
+	RoleId integer,
+	Displayname text,
+	Street1 text,
+	Street2 text,
+	Postcode text,
+	City text,
+	State text,
+	Country text,
+	Point integer,
+	foreign key(RoleId) references Roles(Id)
 );
 
-create table if not exists categories(
-	category varchar(30) primary key
+CREATE TABLE IF NOT EXISTS Categories(
+	Id integer primary key autoincrement,
+	Name varchar(30) NOT NULL
 );
 
-create table if not exists statuses(
-	status varchar(30) primary key
+CREATE TABLE IF NOT EXISTS Statuses(
+	Id integer primary key autoincrement,
+	Name varchar(30) NOT NULL
 );
 
-create table if not exists menus(
-	id	integer primary key autoincrement,
-	category varchar(30),
-	code text,
-	name text,
-	price real,
-	foreign key(category) references categories(category)
+CREATE TABLE IF NOT EXISTS Menus(
+	Id	integer primary key autoincrement,
+	CategoryId integer,
+	Code text NOT NULL,
+	Name text,
+	Price real,
+	foreign key(CategoryId) references Categories(Id)
 );
 
 -- stored charge percentage
-create table if not exists charges(
-	charge varchar(30) primary key,
-	value real
+CREATE TABLE IF NOT EXISTS Charges(
+	Id integer primary key autoincrement,
+	Name varchar(30),
+	Value real
 );
 
-create table if not exists ordertypes(
-	type varchar(30) primary key
+CREATE TABLE IF NOT EXISTS OrderTypes(
+	Id integer primary key autoincrement,
+	Name varchar(30) NOT NULL
 );
 
-create table if not exists orders(
-	id integer primary key autoincrement,
-	created datetime,
-	tableno smallint,
-	member varchar(30),
-	total real,
-	foreign key(member) references users(username)
+CREATE TABLE IF NOT EXISTS Orders(
+	Id integer primary key autoincrement,
+	CreatedById integer NOT NULL,
+	Created datetime NOT NULL,
+	DODate datetime,
+	ReceiptDate datetime,
+	TableNo text,
+	MemberId integer,
+	Total real NOT NULL,
+	foreign key(CreatedById) references Users(Id)
 );
-create table if not exists orderitems(
-	id integer primary key autoincrement,
-	parent integer not null,
-	menu integer not null,
-	status varchar(30) not null,
-	foreign key(parent) references orders(id),
-	foreign key(menu) references menus(id),
-	foreign key(status) references statuses(status)
+
+CREATE TABLE IF NOT EXISTS OrderItems(
+	Id integer primary key autoincrement,
+	ParentId integer not null,
+	MenuId integer not null,
+	StatusId integer not null,
+	foreign key(ParentId) references Orders(Id),
+	foreign key(MenuId) references Menus(Id),
+	foreign key(StatusId) references Statuses(Id)
 );
 
 
 -- insert default value
-insert into roles(role) values('admin');
-insert into roles(role) values('cashier');
-insert into roles(role) values('staff');
-insert into roles(role) values('guest');
+insert into Roles(Name) values('Admin');
+insert into Roles(Name) values('Cashier');
+insert into Roles(Name) values('Staff');
+insert into Roles(Name) values('Guest');
 
-insert into categories(category) values('set');
-insert into categories(category) values('food');
-insert into categories(category) values('drink');
-insert into categories(category) values('dessert');
+insert into Categories(Name) values('Set');
+insert into Categories(Name) values('Food');
+insert into Categories(Name) values('Beverage');
+insert into Categories(Name) values('Dessert');
 
-insert into ordertypes(type) values('dine in');
-insert into ordertypes(type) values('take out');
+insert into OrderTypes(Name) values('Dine in');
+insert into OrderTypes(Name) values('Take out');
 
-insert into statuses(status) values('confirm');
-insert into statuses(status) values('complete');
+insert into Statuses(Name) values('Confirm');
+insert into Statuses(Name) values('Complete');
