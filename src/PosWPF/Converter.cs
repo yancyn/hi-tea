732c7f62b4ba528d9f85d0afaa3c365cd0727aff
yancyn <yancyn@gmail.com>
2013-10-22 18:20:39 +0800

    Added menu display name.

---
+++ src/PosWPF/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using HiTea.Pos;
+
+namespace PosWPF
+{
+    public class MenuDisplayNameConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if(value is HiTea.Pos.Menu)
+            {
+                HiTea.Pos.Menu menu = (HiTea.Pos.Menu)value;
+                return menu.Code + " " + menu.Name;
+            }
+
+            throw new ArgumentException("Not supported type of " + value.GetType());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
