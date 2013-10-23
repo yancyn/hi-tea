974f935e3fa80b206581e426fa80712de4c04152
yancyn <yancyn@gmail.com>
2013-10-23 18:11:03 +0800

    Draft login screen.

---
+++ src/PosWPF/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HiTea.Pos;
+
+namespace PosWPF
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+            // test
+            //PosManager posManager = new PosManager();
+            //HiTea.Pos.User cashier = new HiTea.Pos.User();
+            //cashier.Username = "yancyn";
+            //posManager.Cashier = cashier;
+
+            //this.DataContext = posManager;
+        }
+    }
+}
