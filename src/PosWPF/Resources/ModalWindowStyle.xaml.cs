5f4d7c0545fdc71d61fbecf57c072efc693ad0f3
yancyn <yancyn@gmail.com>
2013-10-31 10:56:27 +0800

    Customized modal window style with back button.

---
+++ src/PosWPF/Resources/ModalWindowStyle.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PosWPF
+{
+    /// <summary>
+    /// Interaction logic for ModalWindow.xaml
+    /// </summary>
+    public partial class ModalWindow : ResourceDictionary
+    {
+        public ModalWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Close modal window");
+        }
+    }
+}
