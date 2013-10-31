b158395a7779fb0b0f4e690b4010a59bf5b1977d
yancyn <yancyn@gmail.com>
2013-10-31 22:45:25 +0800

    Draft on screen numpad.

---
+++ src/PosWPF/Resources/Numpad.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 10/31/2013
+ * Time: 21:55
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace PosWPF
+{
+	/// <summary>
+	/// Interaction logic for Numpad.xaml
+	/// </summary>
+	public partial class Numpad : Window
+	{
+		public Numpad()
+		{
+			InitializeComponent();
+		}
+	}
+}
