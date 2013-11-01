6d805fa7130e3c3691c1b493967fb43b9bb8fa98
yancyn <yancyn@gmail.com>
2013-11-01 12:09:30 +0800

    Able to enter cash by numpad and calculate the return value.

--- src/PosWPF/Resources/Numpad.xaml.cs
+++ src/PosWPF/Resources/Numpad.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using HiTea.Pos;
 
 namespace PosWPF
 {
@@ -31,6 +32,47 @@ namespace PosWPF
         private void EnterButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Order order = (Order)this.DataContext;
+            int value = Convert.ToInt32((sender as Button).Content);
+            if (order.Cash == 0)
+            {
+                order.Cash = value;
+            }
+            else
+            {
+                // TODO: Buggy
+                decimal old = decimal.Round(order.Cash, 2);
+                string cashString = order.Cash.ToString();
+                switch (cashString.Length)
+                {
+                    case 1:
+                        order.Cash = old * 10 + value;
+                        break;
+                    case 2:
+                        order.Cash = old + value / 10m;
+                        break;
+                    case 4:
+                        order.Cash = old + value / 100m;
+                        break;
+                    default:
+                        order.Cash = old * 10 + value / 100m;
+                        break;
+                }
+
+                //int ratio = Math.Max(1, cashString.Length - 1);
+                //order.Cash = order.Cash * ratio * 10 + value / ratio * 10m;
+            }
+
+            System.Diagnostics.Debug.WriteLine(order.Cash);
+        }
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Order order = (Order)this.DataContext;
+            order.Cash = decimal.Round(order.Cash / 10m, 2);
         }
 	}
 }
