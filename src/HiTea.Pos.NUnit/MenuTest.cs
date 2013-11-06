aff3f65e4b324fbfd70778997ea4864b49d5be6d
yancyn <yancyn@gmail.com>
2013-11-06 18:40:20 +0800

    Break menu name into two lines by different language.

--- src/HiTea.Pos.NUnit/MenuTest.cs
+++ src/HiTea.Pos.NUnit/MenuTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 using System.Data.Linq;
 using System.Data.SQLite;
@@ -27,6 +28,24 @@ namespace HiTea.Pos.NUnit
             db = new Main(connectionString);
         }
 
+        private void Add(string code, string cat)
+        {
+            int expected = db.Menus.Count() + 1;
+
+            HiTea.Pos.Menu menu = new HiTea.Pos.Menu();
+            menu.Code = code + new Random().Next(20);
+            menu.Name = foods[new Random().Next(foods.Length)] + new Random().Next(20);
+            menu.Price = (float)Math.Round(new Random().Next(100) / 13F, 2);
+            menu.Active = true;
+            Category category = db.Categories.Where(c => c.Name == cat).FirstOrDefault();
+            if (category != null) menu.CategoryID = category.ID;
+            db.Menus.InsertOnSubmit(menu);
+            db.SubmitChanges();
+
+            int actual = db.Menus.Count();
+            Assert.AreEqual(expected, actual);
+        }
+
         [Test]
         public void AddSetMealTest()
         {
@@ -40,29 +59,39 @@ namespace HiTea.Pos.NUnit
         [Test]
         public void AddBeverageTest()
         {
-            Add("B", "Beverage");
+            Add("D", "Drink");
         }
         [Test]
         public void AddDessertTest()
         {
-            Add("D", "Dessert");
+            Add("E", "Dessert");
         }
 
-        private void Add(string code, string cat)
+        [Test]
+        public void MenuNameTest()
         {
-            int expected = db.Menus.Count() + 1;
+            Menu menu = new Menu();
+            menu.Code = "A01";
+            menu.Name = "上海叉烧鸡饭 SHANGHAI BBQ CHICKEN RICE";
 
-            HiTea.Pos.Menu menu = new HiTea.Pos.Menu();
-            menu.Code = code + new Random().Next(20);
-            menu.Name = foods[new Random().Next(foods.Length)] + new Random().Next(20);
-            menu.Price = (float)Math.Round(new Random().Next(100) / 13F, 2);
-            Category category = db.Categories.Where(c => c.Name == cat).FirstOrDefault();
-            if (category != null) menu.CategoryID = category.ID;
-            db.Menus.InsertOnSubmit(menu);
-            db.SubmitChanges();
+            string expected = "A01 上海叉烧鸡饭\nSHANGHAI BBQ CHICKEN RICE";
+            string actual = string.Empty;
+            actual += menu.Code + " ";
 
-            int actual = db.Menus.Count();
+            // extract English character only
+            string eng = string.Empty;
+            //Match match = Regex.Match(menu.Name, "[a-zA-Z0-9 ]");
+            Regex regex = new Regex("[a-zA-Z0-9 ]");
+            foreach (Match match in regex.Matches(menu.Name))
+                eng += match.Value;
+            string other = menu.Name.Replace(eng, string.Empty);
+            actual += other.Trim() + "\n" + eng.Trim();
+
+            System.Diagnostics.Debug.WriteLine("Eng: " + eng);
+            System.Diagnostics.Debug.WriteLine("Other: " + other);            
             Assert.AreEqual(expected, actual);
         }
+
+
     }
 }
