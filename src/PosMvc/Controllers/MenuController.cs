eb6cb36a18edfa024a4eebeafecb826234f56538
yancyn <yancyn@gmail.com>
2013-11-16 18:11:12 +0800

    Setup menu listing in mvc.

--- src/PosMvc/Controllers/MenuController.cs
+++ src/PosMvc/Controllers/MenuController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HiTea.Pos;
 
 namespace PosMvc.Controllers
 {
@@ -13,7 +15,10 @@ namespace PosMvc.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            string connectionString = ConfigurationManager.ConnectionStrings["PosConnectionString"].ConnectionString;
+            Main db = new Main(connectionString);
+            var menus = db.Menus;
+            return View(menus.ToList());
         }
 
     }
