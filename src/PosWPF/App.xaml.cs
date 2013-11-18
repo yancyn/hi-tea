5a41029c3b43c41b83a0b85256d345a603639b51
yancyn <yancyn@gmail.com>
2013-11-18 14:55:35 +0800

    Error handling at global application level.

--- src/PosWPF/App.xaml.cs
+++ src/PosWPF/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Data;
 using System.Xml;
 using System.Configuration;
+using log4net;
 
 namespace PosWPF
 {
@@ -11,5 +12,27 @@ namespace PosWPF
 	/// </summary>
 	public partial class App : Application
 	{
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            base.OnStartup(e);
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            try
+            {
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+        }
 	}
 }
