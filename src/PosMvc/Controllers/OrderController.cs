1aed3b5c541c6db213a8294f0bf265c03f9f48eb
yancyn <yancyn@gmail.com>
2013-12-22 17:08:27 +0800

    Add order listing in web ordering module.

---
+++ src/PosMvc/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HiTea.Pos;
+
+namespace PosMvc.Controllers
+{
+    public class OrderController : Controller
+    {
+        private Main db = new Main(ConfigurationManager.ConnectionStrings["PosConnectionString"].ConnectionString);
+
+        //
+        // GET: /Order/
+
+        public ActionResult Index()
+        {
+            return View(db.Orders.ToList());
+        }
+
+    }
+}
